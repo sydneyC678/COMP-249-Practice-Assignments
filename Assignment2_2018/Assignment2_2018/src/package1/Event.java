abec80995ba0eed0780878249d3347aef8cd149f
Sydney Campbell <sydcampbell@hotmail.com>
2023-01-02 19:03:12 -0500

    Add files

---
+++ Assignment2_2018/Assignment2_2018/src/package1/Event.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Event {
+    private int year;
+    private int month;
+    private int numOfCities;
+
+    public Event() //default constructor
+    {
+
+    }
+}
